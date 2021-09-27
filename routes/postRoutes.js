7163b62874117fd953a40bc39b3b11521265b405
aayvazyan <manchayvazyan@gmail.com>
2021-09-27 17:50:38 +0400

    minor change

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router  = express.Router();
 const postsController = require('../controllers/postsController');
+const commentsController = require('../controllers/commentsController');
 
-router.post('', postsController.createPost);
-router.get('', postsController.getUserPosts);
-router.get('/:id', postsController.getPostById);
-router.put('/:id', postsController.updatePostById);
-router.delete('/:id', postsController.deletePostById);
+const comments = require('./commenttRoutes')
+
+
+const auth = require('../middleware/auth');
+
+router.post('', auth, postsController.createPost);
+router.get('', auth, postsController.getUserPosts);
+router.get('/:id', auth, postsController.getPostById);
+router.put('/:id', auth, postsController.updatePostById);
+router.delete('/:id', auth, postsController.deletePostById);
+router.post('/:id/comments', auth, commentsController.createComment);
 
 module.exports = router;
